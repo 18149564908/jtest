f606ce36a9ed462ccda143fad81440b3ce97ff4e
1091820468 <1091820468@qq.com>
2019-10-17 22:05:18 +0800

    '引入antd'

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import styles from './test.less'
+import { Button } from 'antd';
+import BasicLayout from './layout/BasicLayout'
 
 class Index extends React.Component{
     render() {
-        return <div className={styles.test}>sss</div>
+        return (
+            <div className={styles.test}>
+                <BasicLayout/>
+            </div>
+        )
     }
 }
 
