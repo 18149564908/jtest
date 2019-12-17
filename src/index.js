d1ed2f0eda8e13205aa1fd3042f1e08b9ef49ffc
1091820468 <1091820468@qq.com>
2019-12-17 20:27:17 +0800

    css modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 import styles from './test.less'
 import { Button } from 'antd';
 import BasicLayout from './layout/BasicLayout'
@@ -10,9 +9,10 @@ import BasicLayout from './layout/BasicLayout'
 class Index extends React.Component{
     render() {
         return (
-            <div className={styles.test}>
+            <div>
                 <BasicLayout>
-                    sss
+                    
+                    <div className={styles.test}>sss</div>
                 </BasicLayout>
             </div>
         )
